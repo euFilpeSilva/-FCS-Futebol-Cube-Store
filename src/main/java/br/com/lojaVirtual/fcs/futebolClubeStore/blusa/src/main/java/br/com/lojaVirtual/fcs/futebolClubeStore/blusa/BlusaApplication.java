fe2f088be34a89ea9e9ce29b7197c2b88f94c444
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-03-26 10:52:38 -0300

    atualizando diretorios

--- src/main/java/br/com/lojaVirtual/fcs/futebolClubeStore/blusa/src/main/java/br/com/lojaVirtual/fcs/futebolClubeStore/blusa/BlusaApplication.java
+++ src/main/java/br/com/lojaVirtual/fcs/futebolClubeStore/blusa/src/main/java/br/com/lojaVirtual/fcs/futebolClubeStore/blusa/BlusaApplication.java
@@ -1,4 +1,4 @@
-package br.com.lojaVirtual.fcs.futebolClubeStore.blusa;
+package br.com.lojaVirtual.fcs.futebolClubeStore.blusa.src.main.java.br.com.lojaVirtual.fcs.futebolClubeStore.blusa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
